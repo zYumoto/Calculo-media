0aaaeca10e4128c1521aa46072005bfbabb6615f
zYumoto <victorjuanrazer@gmail.com>
2023-11-07 19:49:46 -0300

    inicialize project

---
+++ js/media.js
@@ -0,0 +1,22 @@
+const p1 = document.getElementById("p1");
+const p2 = document.getElementById("p2");
+const btnCalcular = document.getElementById("btnCalcular");
+const saida = document.getElementById("saida");
+
+saida.textContent = "Oi!!";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//document.getElementById("saida").textContent = "Oi!";
